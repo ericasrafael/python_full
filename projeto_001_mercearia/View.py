d1c7f6fc3be6b195ff3c8358c9fe2cb92bc8415f
eritools <ericasrafael@gmail.com>
2022-08-23 08:15:55 -0300

    implementação da View

--- projeto_001_mercearia/View.py
+++ projeto_001_mercearia/View.py
@@ -4,7 +4,7 @@
 import Controller
 import os.path
 
-# cao no if apenas se arquivos não existirem
+# cai no if apenas se arquivos não existirem
 
 
 def criar_arquivos(*args):
@@ -18,16 +18,17 @@ criar_arquivos("categoria.txt", "estoque.txt",
                "clientes.txt", "fornecedores.txt",
                "funcionarios.txt", "venda.txt")
 
+
 # só é verdadeiro se este arquivo .py for executado nele mesmo, ou seja,
 # se a view for importada para outro arquivo .py o if retornaria False
 if __name__ == "__main__":
     while True:
-        local = int(input("Digite 1 para acessar [ Categorias ]\n"
-                          "Digite 2 para acessar [ Estoque ] \n"
-                          "Digite 3 para acessar [ Fornecedor ]\n"
-                          "Digite 4 para acessar [ Clientes ]\n"
-                          "Digite 5 para acessar [ Funcionario ]\n"
-                          "Digite 6 para acessar [ Vendas ]\n"
+        local = int(input("Digite 1 para acessar Categorias\n"
+                          "Digite 2 para acessar Estoque\n"
+                          "Digite 3 para acessar Fornecedor\n"
+                          "Digite 4 para acessar Clientes\n"
+                          "Digite 5 para acessar Funcionario\n"
+                          "Digite 6 para acessar Vendas\n"
                           "Digite 7 para acessar os produtos mais vendidos\n"
                           "Digite 8 para sair\n"))
 
@@ -88,7 +89,7 @@ if __name__ == "__main__":
                         "Digite o produto para qual deseja alterar:\n")
                     novo_preco = input("Digite o novo preco do produto:\n")
                     nova_categoria = input(
-                        "Digite a nova cayegoria do produto:\n")
+                        "Digite a nova categoria do produto:\n")
                     nova_quantidade = input(
                         "Digite a nova quantidade do produto")
                     est.alterar(produto_alterar, novo_produto,
@@ -102,9 +103,9 @@ if __name__ == "__main__":
         elif local == 3:
             forn = Controller.ConFornecedor()
             while True:
-                decisao = int(input("Digite 1 para cadastrar uma fornecedor\n"
-                                    "Digite 2 para remover uma fornecedor\n"
-                                    "Digite 3 para alterar uma fornecedor\n"
+                decisao = int(input("Digite 1 para cadastrar um fornecedor\n"
+                                    "Digite 2 para remover um fornecedor\n"
+                                    "Digite 3 para alterar um fornecedor\n"
                                     "Digite 4 para visualizar fornecedores cadastrados\n"
                                     "Digite 5 para sair\n"))
                 if decisao == 1:
@@ -125,7 +126,7 @@ if __name__ == "__main__":
                         "Digite o produto para qual deseja alterar:\n")
                     novo_cnpj = input("Digite o novo preco do produto:\n")
                     novo_contato = input(
-                        "Digite a nova cayegoria do produto:\n")
+                        "Digite a nova categoria do produto:\n")
                     nova_categoria = input(
                         "Digite a nova quantidade do produto")
                     forn.alterar(alterar_fornecedor, novo_fornecedor,
@@ -139,8 +140,8 @@ if __name__ == "__main__":
             cli = Controller.ConCliente()
             while True:
                 decisao = int(input("Digite 1 para cadastrar um cliente\n"
-                                    "Digite 2 para remover uma cliente\n"
-                                    "Digite 3 para alterar uma cliente\n"
+                                    "Digite 2 para remover um cliente\n"
+                                    "Digite 3 para alterar um cliente\n"
                                     "Digite 4 para visualizar clientes cadastrados\n"
                                     "Digite 5 para sair\n"))
                 if decisao == 1:
@@ -162,7 +163,7 @@ if __name__ == "__main__":
                     novo_contato = input("Digite o novo contato do cliente:\n")
                     novo_cpf = input("Digite a novo CPF do cliente:\n")
                     novo_email = input("Digite o novo e-mail do cliente")
-                    nova_endereco = input("Digite o novo endereco do produto")
+                    nova_endereco = input("Digite o novo endereco do cliente")
                     cli.alterar(alterar_cliente, novo_cliente,
                                 novo_contato, novo_cpf, novo_email, nova_endereco)
                 elif decisao == 4:
@@ -183,8 +184,9 @@ if __name__ == "__main__":
                     contato = input("Digite o contato do novo funcionario:\n")
                     cpf = input("Digite o CPF do novo funcionario:\n")
                     email = input("Digite o e-mail do novo funcionario:\n")
-                    endereco = input("Digite a endereco do novo funcionario:\n")
-                    func.cadastrar(clt,nome,contato,cpf,email, endereco)
+                    endereco = input(
+                        "Digite a endereco do novo funcionario:\n")
+                    func.cadastrar(clt, nome, contato, cpf, email, endereco)
                 elif decisao == 2:
                     remover_funcionario = input(
                         "Digite o nome do funcionario que deseja remover:\n")
@@ -195,11 +197,15 @@ if __name__ == "__main__":
                     novo_funcionario = input(
                         "Digite o nome do funcionario para qual deseja alterar:\n")
                     novo_clt = input("Digite a nova CLT do funcionario:\n")
-                    novo_contato = input("Digite o novo contato do funcionario:\n")
+                    novo_contato = input(
+                        "Digite o novo contato do funcionario:\n")
                     novo_cpf = input("Digite a novo CPF do funcionario:\n")
-                    novo_email = input("Digite o novo e-mail do funcionario:\n")
-                    nova_endereco = input("Digite o novo endereco do funcionario: ")
-                    func.alterar(alterar_funcionario, novo_funcionario, novo_clt, novo_contato, novo_cpf, novo_email, nova_endereco)
+                    novo_email = input(
+                        "Digite o novo e-mail do funcionario:\n")
+                    nova_endereco = input(
+                        "Digite o novo endereco do funcionario: ")
+                    func.alterar(alterar_funcionario, novo_funcionario, novo_clt,
+                                 novo_contato, novo_cpf, novo_email, nova_endereco)
                 elif decisao == 4:
                     func.visualizar()
                 else:
@@ -217,8 +223,10 @@ if __name__ == "__main__":
                     quantidade = input("Digite a quantidade vendida:\n")
                     vend.cadastrar(produto, vendedor, cliente, quantidade)
                 elif decisao == 2:
-                    dataInicio = input("Digite a data de inicio no formato dia/mes/ano: \n")
-                    dataTermino = input("Digite a data de termino no formato dia/mes/ano: \n")
+                    dataInicio = input(
+                        "Digite a data de inicio no formato dia/mes/ano: \n")
+                    dataTermino = input(
+                        "Digite a data de termino no formato dia/mes/ano: \n")
                     vend.visualizar(dataInicio, dataTermino)
                 else:
                     break
@@ -227,4 +235,3 @@ if __name__ == "__main__":
             vend.relatorioVendas()
         else:
             break
-            
